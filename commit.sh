f5b3fd9870ff7a9aaf5dea8e1cd3b4bcec88442f
ZonglinL <zonglinlyu123123@gmail.com>
2025-01-24 21:21:42 -0500

    intial commit

--- commit.sh
+++ commit.sh
@@ -7,6 +7,6 @@ git add .
 #git add README.md
 #git add train/reward_control_gradient_straight.py 
 git commit -m "intial commit"
-git remote add https://github.com/ZonglinL/TLBVFI
+git remote add origin https://github.com/ZonglinL/TLBVFI
 git branch -M main
 git push -u origin main
